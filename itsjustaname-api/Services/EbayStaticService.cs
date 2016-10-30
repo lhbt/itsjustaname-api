10d92d1f5fb180513e1ff8dee543537408f1d98d
Laurent Humbert <Laurent.Humbert@codecomputerlove.com>
2016-10-30 03:19:20 +0000

    new ebay service, static, to not consume api calls

---
+++ itsjustaname-api/Services/EbayStaticService.cs
@@ -0,0 +1,21 @@
+﻿using itsjustaname_api.Models;
+using itsjustaname_api.Services.Interfaces;
+
+namespace itsjustaname_api.Services
+{
+    public class EbayStaticService : IEbayService
+    {
+        public EbayProductModel GetEbayProduct(string keyword)
+        {
+            return new EbayProductModel
+            {
+                Price = 219.99,
+                ImageUrl = "http://thumbs4.ebaystatic.com/m/m-t38kJKKUVaDGi309wo2JA/140.jpg",
+                ItemUrl =
+                    "http://www.ebay.co.uk/itm/Beko-WMB61432B-Free-Standing-6Kg-1400-Spin-Slim-Depth-Washing-Machine-Black-/361504943331",
+                Name = "Beko WMB61432B Free Standing 6Kg 1400 Spin Slim Depth Washing Machine - Black",
+                BigImageUrl = "http://galleryplus.ebayimg.com/ws/web/361504943331_1_33_1.jpg"
+            };
+        }
+    }
+}
